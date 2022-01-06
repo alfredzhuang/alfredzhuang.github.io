63cbae409d659cd44c85196884f999e0b2fc3a57
alfredzhuang <alfredzhuang123@gmail.com>
2022-01-06 13:05:33 -0800

    implement navBar

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,13 @@ window.addEventListener("DOMContentLoaded", (e) => {
     splash.classList.add("no-display");
   }, 2000);
 });
+
+function renderAbout() {
+  about.classList.remove("remove");
+  projects.classList.add("remove");
+}
+
+function renderProjects() {
+  about.classList.add("remove");
+  projects.classList.remove("remove");
+}
