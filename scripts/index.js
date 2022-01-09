fa6e574143eae48c177b25f81d5dd9886049f39d
alfredzhuang <alfredzhuang123@gmail.com>
2022-01-08 16:58:37 -0800

    create a footer

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,14 @@ const splash = document.querySelector(".splash");
 const navbar = document.querySelector("#navbar");
 const about = document.querySelector("#about");
 const projects = document.querySelector("#projects");
+const footer = document.querySelector("#footer");
 
 window.addEventListener("DOMContentLoaded", (e) => {
   setTimeout(() => {
     splash.classList.add("no-display");
     navbar.classList.remove("hidden");
     about.classList.remove("hidden");
+    footer.classList.remove("hidden");
   }, 2000);
 });
 
