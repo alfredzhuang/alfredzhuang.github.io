d6b0f90f9ff7f582019e17bd9dd1ad7bbe5e3505
alfredzhuang <alfredzhuang123@gmail.com>
2022-01-10 13:44:31 -0800

    add template for about page

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
 const splash = document.querySelector(".splash");
 const navbar = document.querySelector("#navbar");
-const about = document.querySelector("#about");
-const projects = document.querySelector("#projects");
+const about = document.querySelector(".about-container");
+const projects = document.querySelector(".projects-container");
 const footer = document.querySelector("#footer");
 
 window.addEventListener("DOMContentLoaded", (e) => {
   setTimeout(() => {
     splash.classList.add("no-display");
     navbar.classList.remove("hidden");
-    about.classList.remove("hidden");
+    about.classList.remove("remove");
     footer.classList.remove("hidden");
   }, 2000);
 });
 
 function renderAbout() {
-  about.classList.remove("hidden");
-  projects.classList.add("hidden");
+  about.classList.remove("remove");
+  projects.classList.add("remove");
 }
 
 function renderProjects() {
-  about.classList.add("hidden");
-  projects.classList.remove("hidden");
+  about.classList.add("remove");
+  projects.classList.remove("remove");
 }
